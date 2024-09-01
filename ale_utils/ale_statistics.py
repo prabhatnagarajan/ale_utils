a6f94b3ebde62bc3000f207bc03348bdb1be3686
Prabhat Nagarajan <prabhat.nagarajan@gmail.com>
2024-08-31 18:02:09 -0600

    Adds computations of statistics regarding hns

--- ale_utils/ale_statistics.py
+++ ale_utils/ale_statistics.py
@@ -1,5 +1,6 @@
 from ale_utils import scores as ale_scores
 from ale_utils import basics
+import statistics
 from pdb import set_trace
 
 def compute_partial_hns_scores(alg_scores):
@@ -15,8 +16,23 @@ def compute_hns_scores(alg_scores):
 	assert all([env in alg_envs for env in basics.ATARI_57])
 	return compute_partial_hns_scores(alg_scores)
 
-def get_hns_statistics(hns_scores, stat='median'):
-	assert stat in ['mean', 'median', 'mode']
+def get_hns_statistics(hns_scores, stat):
+	assert stat in ['mean', 'median', 'mode', 'iqm']
+	sorted_hns_scores = sorted(list(hns_scores.values()))
+	if stat == 'mean':
+		return statistics.mean(sorted_hns_scores)
+	elif stat == 'median':
+		return statistics.median(sorted_hns_scores)
+	elif stat == 'mode':
+		return statistics.mode(sorted_hns_scores)
+	else:
+		cut_points = statistics.quantiles(sorted_hns_scores, n=4)
+		q1, q3 = cut_points[0], cut_points[2]
+		iqr_data = [hns for hns in sorted_hns_scores if q1 <= hns <= q3]
+		iqm = statistics.mean(iqr_data)
+		return iqm
 
 if __name__ == '__main__':
-	compute_hns_scores(ale_scores.HUMAN_SCORES_ATARI_57) # should be ones
+	hns_scores = compute_hns_scores(ale_scores.HUMAN_SCORES_ATARI_57) # should be ones
+	get_hns_statistics(hns_scores, 'iqm')
+
